2f17db4764ff70f2a0c170a13bd08bb0994d3532
ekatrif <katiapervak@gmail.com>
2022-08-16 11:09:57 +0300

    Добавлен функционал кнопки Купить

--- app.js
+++ app.js
@@ -1,70 +1,63 @@
-let productService
-const cartService = new CartService()
-const htmlService = new HTMLService()
+let productService;
+const cartService = new CartService();
+const htmlService = new HTMLService();
 
-const productsContainer = document.getElementById('products')
-const cartContainer = document.getElementById('cart')
-const filterInput = document.getElementById('filter')
+const productsContainer = document.getElementById("products");
+const cartContainer = document.getElementById("cart");
+const filterInput = document.getElementById("filter");
 
-filterInput.addEventListener('input', event => {
-  const value = event.target.value
+filterInput.addEventListener("input", (event) => {
+  const value = event.target.value;
 
-  const filteredProducts = productService.filterBy(value)
+  const filteredProducts = productService.filterBy(value);
 
-  renderProducts(filteredProducts)
-})
+  renderProducts(filteredProducts);
+});
 
-productsContainer.addEventListener('click', event => {
-  const id = event.target.dataset.id
-    ? event.target.dataset.id
-    : event.target.closest('li')?.dataset.id
-
-  if (id) {
-    cartService.add(
-      productService.getById(+id)
-    )
-    renderCart()
+productsContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("buy")) {
+    const id = event.target.closest("li")?.dataset.id;
+    cartService.add(productService.getById(+id));
+    renderCart();
   }
-})
+});
 
-cartContainer.addEventListener('click', event => {
-  const type = event.target?.dataset.type
-  const id = event.target?.dataset.id
+cartContainer.addEventListener("click", (event) => {
+  const type = event.target?.dataset.type;
+  const id = event.target?.dataset.id;
 
   switch (type) {
-    case 'clear':
-      cartService.clear()
-      renderCart()
-      break
-    case 'remove':
-      cartService.remove(id)
-      renderCart()
-      break
+    case "clear":
+      cartService.clear();
+      renderCart();
+      break;
+    case "remove":
+      cartService.remove(id);
+      renderCart();
+      break;
   }
-})
+});
 
 function renderProducts(products) {
-  productsContainer.innerHTML = htmlService.paintProducts(products)
+  productsContainer.innerHTML = htmlService.paintProducts(products);
 }
 
 function renderCart() {
-  cartContainer.innerHTML = htmlService.paintCart(
-    cartService.getInfo()
-  )
+  cartContainer.innerHTML = htmlService.paintCart(cartService.getInfo());
 }
 
 async function startApplication() {
-  renderCart()
+  renderCart();
   try {
-    const response = await fetch('https://fakestoreapi.com/products')
-    const data = await response.json()
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
 
-    productService = new ProductService(data)
+    productService = new ProductService(data);
 
-    renderProducts(productService.products)
+    renderProducts(productService.products);
   } catch (e) {
-    productsContainer.innerHTML = htmlService.paintError(e)
+    productsContainer.innerHTML = htmlService.paintError(e);
   }
 }
 
-startApplication()
+startApplication();
